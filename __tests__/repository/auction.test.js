5621a0f971d79c50e09830bc92b7719486b9d815
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-22 19:31:47 -0300

    test(git): customizing the implementation

--- __tests__/repository/auction.test.js
+++ __tests__/repository/auction.test.js
@@ -1,15 +1,50 @@
 import { getAuctions } from '../../src/repository/auction';
+import apiAuctions from '../../src/services/apiAuctions';
 
 jest.mock('../../src/services/apiAuctions');
+
+const mockAuctions = [
+  {
+    id: 1,
+    name: 'Auction',
+    description: 'Auction description'
+  }
+];
+
+const mockRequest = (retorno) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: retorno
+      }, 200);
+    });
+  });
+}
+
+const mockRequestErro = () => {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject();
+    }, 200);
+  })
+}
 
 describe('repository/auction', () => {
 
   describe('getAuctions', () => {
 
-    it('Deve retornar uma lista de leilões', async () => {
+    it('Should return a list of auctions', async () => {
+      apiAuctions.get.mockImplementation(() => mockRequest(mockAuctions));
 
       const auctions = await getAuctions();
-      console.log(auctions);
+      expect(auctions).toEqual(mockAuctions)
+    });
+
+    it('Must return an empty list when the request fails', async () => {
+      apiAuctions.get.mockImplementation(() => mockRequestErro());
+
+      const auctions = await getAuctions();
+      expect(auctions).toEqual([]);
     });
   });
 });
